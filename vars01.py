eae5360cba4fcada34a2bdaa25d6cf8efffe0785
Gabrijel <g.gordic@outlook.com>
2023-02-16 21:16:00 +0100

    a

--- vars01.py
+++ vars01.py
@@ -10,3 +10,16 @@ full_name = 'Pero "Petar" Peric'    # drugi nacin (ovaj je bolji brzi i jednosta
 
 # print(first_name, last_name)
 print(full_name)
+
+year_of_birth = 1975 
+country_of_birth = "Hrvatska"
+employement_status = True
+weight = 95
+gender = "M"
+
+# stranice trokuta te povrsina
+
+a = "6"
+b = 3
+surface = a * b 
+print(surface)
