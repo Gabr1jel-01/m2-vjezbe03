93a530f4920ad3232c482060203fe0c15b8e735f
Gabrijel <g.gordic@outlook.com>
2023-02-16 20:38:03 +0100

    Create vars01.py

---
+++ vars01.py
@@ -0,0 +1,12 @@
+# Kreirajte varijable (imenujte ih i dodijelite im odgovarajucu vrijednost) za:
+# Ime, prezime , godine rodenja, status radnog odnosa, tezinu te spol
+# Stranica a i b cetverokuta te za povrsinu 
+
+first_name = "Pero"
+last_name = 'Peric'
+full_name = "Pero \"Petar\" Peric" # prvi nacin
+full_name = 'Pero "Petar" Peric'    # drugi nacin (ovaj je bolji brzi i jednostavniji meni)
+
+
+# print(first_name, last_name)
+print(full_name)
